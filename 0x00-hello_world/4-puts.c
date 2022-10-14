da96d5468bbbeaabf308fcab0592a54e3be773c9
teneng24 <tenengchristopher@gmail.com>
2022-10-14 03:32:26 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(void)
+
 	/**
 	 * main - Entry Point
+	 *
 	 * Return: Always 0 (Success)
-	 **/
+	 */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
