0663e8765eebf890883189c279365284ee61fb2b
teneng24 <tenengchristopher@gmail.com>
2022-10-14 03:14:46 -0700

    .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	char first[] = "Programming is like building a multilingual puzzle";
+
+	puts(first);
+	return (0);
+}
---
+++ 0x00-hello_world/4-putsss.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	char first[]="Programming is like building a multilingual puzzle";
+	puts(first);
+	return 0;
+}
