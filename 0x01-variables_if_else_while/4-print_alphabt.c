34a4b2db153f6beee78ee9588560ed041bff4ead
teneng24 <tenengchristopher@gmail.com>
2022-10-17 16:30:03 -0700

    .

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,10 @@ char low, e, q;
 e = 'e';
 q = 'q';
 for (low = 'a'; low <= 'z'; low++)
-{										if (low != e && low != q)
+{
+if (low != e && low != q)
 putchar(low);
 }
 putchar('\n');
 return (0);
-}   
+}
