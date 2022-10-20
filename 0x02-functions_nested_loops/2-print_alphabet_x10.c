67f6772c0ef975390a21f5e40ddc2fda913d5f41
teneng24 <tenengchristopher@gmail.com>
2022-10-20 08:26:40 -0700

    .

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print's 10 times the alphabet.
+ *
+ * Return: lower
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int lower = 0;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == c)
+lower = 1;
+}
+return (lower);
+}
