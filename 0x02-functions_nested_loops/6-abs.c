42300527e664fb5a1cc30f33bae231d16fdcee0f
teneng24 <tenengchristopher@gmail.com>
2022-10-20 09:57:35 -0700

    .

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: An integre input
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+if (r >= 0)
+return (r);
+else
+return (r * -1);
+}
