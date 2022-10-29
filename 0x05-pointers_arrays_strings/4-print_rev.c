4edaf66d32607a21a446645905ffdee8a9c4a3b6
teneng24 <tenengchristopher@gmail.com>
2022-10-29 00:21:58 -0700

    .

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * main - check the code for Holberton School students.
- * Return: Always 0.
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
  */
 void print_rev(char *s)
 {
@@ -12,5 +14,3 @@ while (len)
 _putchar(s[--len]);
 _putchar('\n');
 }
-
-
