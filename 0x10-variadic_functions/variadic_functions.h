78a7e69eef98be4bb954888fd97bf58c453dd1e0
teneng24 <tenengchristopher@gmail.com>
2022-11-17 04:21:48 -0800

    .

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 int _putchar(int);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
